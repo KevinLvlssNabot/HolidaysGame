9984b9c5eb2898801cb6d4d8cc03a9c9b4008f82
NabotKevin <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-02-17 19:48:11 +0100

    fin premier combat

--- Loveless_Dancer_Game.c
+++ Loveless_Dancer_Game.c
@@ -28,7 +28,7 @@ typedef struct Perso_t perso;
 struct Zone_t{
   char nom[30];
   int salleMax;
-  char salle[20][30];
+  char salle[100][30];
 };
 typedef struct Zone_t zone;
 
@@ -150,22 +150,79 @@ int combat1v1(int pv, int pvM, char nom[20], char nomM[20], int choixAction, int
     }
 }
 
+//Apparition Monstre
+void apparitionM(char nomM[20]){
+    printf("%s apparait.\n", nomM);
+}
+
+// Choix action personnage
+int actionP(char nom[20], int choixAction){
+  printf("Que dois faire %s ?\n", nom);
+  printf("Attaquer [0], Defendre [1], Magie [2], Objets [3]\n");
+  scanf("%d", &choixAction);
+    return choixAction;
+}
+
+// Action M
+int actioM(int MAX, int MIN, int actionM){
+  MAX = 1, MIN = 0;
+  actionM = (rand() %(MAX - MIN + 1)) + MIN;
+    return actionM;
+}
+
+// Perso Attaque
+int choixIsAtt(char nom[20], char nomM[20], int attaqueCombat, int pvM){
+      printf("%s attaque.\n", nom);
+              printf("%s subit %d degats.\n", nomM, attaqueCombat);
+              pvM = pvM - attaqueCombat;
+      return pvM;
+}
+
+// Perso defend
+int choixIsDef(int attaqueCombatM, char nomM[20]){
+    attaqueCombatM = attaqueCombatM/2;
+    printf("%s se defend.\n", nomM);
+    return attaqueCombatM;
+}
+
+// M Attaque
+int choixMIsAtt(char nomM[20], char nom[20], int attaqueCombatM, int pv){
+      printf("%s attaque.\n", nomM);
+              printf("%s subit %d degats.\n", nom, attaqueCombatM);
+              pv = pv - attaqueCombatM;
+      return pv;
+}
+
+// M defend
+int choixMIsDef(int attaqueCombat, char nom[20]){
+    attaqueCombat = attaqueCombat/2;
+    printf("%s se defend.\n", nom);
+    return attaqueCombat;
+}
+
+// Calcul xp gagnée
 int expWin(char nom[20], int xp, int levelup, int niveau, int xpM){
       printf("Vous remportez %d points d'experience.\n", xpM);
       xp = xp + xpM;
     printf("%s Nv.%d %d / %d\n", nom, niveau, xp, levelup);
     return xp;
 }
-
+// Verification de lvl up
 int oneUp(char nom[20], int xp, int levelup, int niveau){
   if (xp >= levelup) {
-    niveau++;
-    printf("%s monte au niveau %d !\n", nom, niveau);
     levelup = levelup*2.5;
   }
     return levelup;
 }
 
+// Affichage du niveau
+int affLevel(char nom[20], int xp, int levelup, int niveau){
+  if (xp >= levelup) {
+    niveau++;
+    printf("%s monte au niveau %d !\n", nom, niveau);
+  }
+    return niveau;
+}
 
 //  Sleep(7000); system("cls"); Color (11,0);
 //  printf("La bas ! Il y a quelqu'un au fond de cette impasse ! Vite amenez-vous !\nEh! Eh! Vous m'entendez ? Reveillez-vous !\nAIDEZ-MOI A LA PORTER, BANDE DE FAINEANTS !\n");
@@ -190,40 +247,8 @@ int main(int argc, char const *argv[]) {
 
     ennemis chauveSouris = {"Chauve-Souris", 20, 5, 5, 15, 1, 0};
 
-while (sortie == 0) {
-    printf("-------------- %s | %s ------------\n", grotteDepart.nom, grotteDepart.salle[position]);
-      if (position < grotteDepart.salleMax && position > 0) {
-          printf("Avancer [0], Reculer[1]\n");
-        } else if (position == 0) {
-          printf("Avancer [0]\n");
-        } else if (position == grotteDepart.salleMax){
-          printf("Reculer[1], Sortir[2]\n");
-        }
-      scanf("%d", &choixDirection);
-        while (choixDirection < 0 || choixDirection > 2) {
-          printf("Mere m'aurait dit: 'chaque chose en son temps, ma fille'.\n");
-          scanf("%d", &choixDirection);
-        }
-            if (choixDirection == 0) {
-              printf("Vous avancez.\n");
-              position++;
-            } else if (choixDirection == 1 && position > 0) {
-              printf("Vous reculez.\n");
-              position = position -1;
-            } else if (choixDirection == 2 && position == grotteDepart.salleMax) {
-              printf("Vous sortez de la zone.\n");
-              sortie = 1;
-            }
-            if (position == 1) {
-              persoPrincipal.pv = combat1v1(persoPrincipal.pv, chauveSouris.pv, persoPrincipal.nom, chauveSouris.nom, persoPrincipal.choixAction, chauveSouris.actionM, chauveSouris.max, chauveSouris.min, finCombat, persoPrincipal.attaqueBase, chauveSouris.attaqueBase, persoPrincipal.attaqueCombat, chauveSouris.attaqueCombat);
-                if (persoPrincipal.pv > 0) {
-              persoPrincipal.exp = expWin(persoPrincipal.nom, persoPrincipal.exp, persoPrincipal.levelup, persoPrincipal.niveau, chauveSouris.expDonnee);
-              persoPrincipal.levelup = oneUp(persoPrincipal.nom, persoPrincipal.exp, persoPrincipal.levelup, persoPrincipal.niveau);
-            } else {
-                return 0;
-            }
-        }
-}
+
+
 
 
 // Ecran d'accueil
@@ -266,9 +291,40 @@ while (sortie == 0) {
               sortie = 1;
             }
             if (position == 1) {
-              combat1v1(persoPrincipal.pv, chauveSouris.pv, persoPrincipal.nom, chauveSouris.nom, persoPrincipal.choixAction, chauveSouris.actionM, chauveSouris.max, chauveSouris.min, finCombat, persoPrincipal.attaqueBase, chauveSouris.attaqueBase, persoPrincipal.attaqueCombat, chauveSouris.attaqueCombat);
+              apparitionM(chauveSouris.nom); while(finCombat == 0){
+                persoPrincipal.choixAction = actionP(persoPrincipal.nom, persoPrincipal.choixAction); chauveSouris.actionM = actioM(chauveSouris.max,chauveSouris.min, chauveSouris.actionM);
+                if (persoPrincipal.choixAction == 0) {
+                    chauveSouris.pv = choixIsAtt(persoPrincipal.nom, chauveSouris.nom, persoPrincipal.attaqueCombat, chauveSouris.pv);
+                } else if (persoPrincipal.choixAction == 1) {
+                  chauveSouris.attaqueCombat = choixIsDef(chauveSouris.attaqueCombat, chauveSouris.nom);
+                }
+                    if (chauveSouris.actionM == 0) {
+                      persoPrincipal.pv = choixMIsAtt(chauveSouris.nom, persoPrincipal.nom, chauveSouris.attaqueCombat, persoPrincipal.pv);
+                    } else if (chauveSouris.actionM == 1) {
+                      persoPrincipal.attaqueCombat = choixMIsDef(persoPrincipal.attaqueCombat, chauveSouris.nom);
+                    }
+                persoPrincipal.attaqueCombat = persoPrincipal.attaqueBase;
+                chauveSouris.attaqueCombat = chauveSouris.attaqueBase;
+                printf("%s | %dPv\n", persoPrincipal.nom, persoPrincipal.pv);
+                printf("%s | %dPV\n", chauveSouris.nom, chauveSouris.pv);
+                if (persoPrincipal.pv <= 0) {
+                  printf("%s tombe KO.\n", persoPrincipal.nom);
+                  printf("GAME OVER.\n");
+                  return 0;
+                } else if (chauveSouris.pv <= 0) {
+                  printf("%s n'a plus de pv.\n", chauveSouris.nom);
+                  finCombat = 1;
+                }
+          }
+                if (persoPrincipal.pv > 0) {
+              persoPrincipal.exp = expWin(persoPrincipal.nom, persoPrincipal.exp, persoPrincipal.levelup, persoPrincipal.niveau, chauveSouris.expDonnee);
+              persoPrincipal.levelup = oneUp(persoPrincipal.nom, persoPrincipal.exp, persoPrincipal.levelup, persoPrincipal.niveau);
+            } else {
+                return 0;
             }
+        }
 }
+
 
 
   return 0;
